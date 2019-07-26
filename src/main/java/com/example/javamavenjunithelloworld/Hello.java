d0fff9b3701ba77a5657f5285d89a5597727bc42
sathishmr <sathish.mr@outlook.com>
2019-07-26 14:11:40 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
  */
 public class Hello {
 
-    static final String HELLO = "Hello!";
+    static final String HELLO = "Hello 2!";
 
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
 
